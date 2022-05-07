42e39d80ef395c5e65ae67cadcf1bb19a06e1e24
Vlad <strenghtword@gmail.com>
2022-05-07 21:41:39 +0300

    Add functionalty to show random charaters, list characters and detail info about characters

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,7 +2,7 @@
 class MarvelService {
 
 _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-_apiKey = 'apikey=fd711f982060d28f8c59a417cdc31a8';
+_apiKey = 'apikey=fd711f982060d28f8c59a417cdc31a88';
 
    getResource = async (url) =>{
        let res = await fetch(url);
@@ -13,27 +13,31 @@ _apiKey = 'apikey=fd711f982060d28f8c59a417cdc31a8';
        return await res.json();
    }
 
-   getAllCharacters = async () =>{
-       const res = this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-       return res.data.results.map(this._transformCharacter);
-   }
+//   getAllCharacters = async () => {
+//         const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+//         return res.data.results.map(this._transformCharacter);
+//     }
+getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter);
+}
 
-   getCharacters = async (id) =>{
-
+getCharacter = async (id) => {
     const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
     return this._transformCharacter(res.data.results[0]);
 }
 
-    _transformCharacter = (char) =>{
-    return{
+_transformCharacter = (char) => {
+    return {
+        id: char.id,
         name: char.name,
-        description: char.description ? `${char.description.slice(0,210)} ...` : 'There is no description for this character',
-        thumbnail:char.thumbnail.path + '.' +char.thumbnail.extension ,
+        description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+        thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
         homepage: char.urls[0].url,
-        wiki: char.urls[1].url  
+        wiki: char.urls[1].url,
+        comics: char.comics.items
     }
-    }
-
-    }
+}
+}
 
 export default MarvelService;
