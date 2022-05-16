aaa7970c49c9fe4dea0beaf5c7280b1e0fe5b78d
Vlad <strenghtword@gmail.com>
2022-05-16 20:06:34 +0300

    Added light seo optimization by Helmet

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,4 +1,6 @@
 
+import {Helmet} from 'react-helmet'
+
 import ComicsList from "../comicsList/ComicsList";
 import AppBanner from "../appBanner/AppBanner";
 
@@ -6,7 +8,14 @@ import AppBanner from "../appBanner/AppBanner";
 const ComicsPage = () => {
 
     return (
-        <>
+    <>
+        <Helmet>
+        <meta
+            name="description"
+            content="Page with list of our comics"
+            />
+            <title>Comics Page</title>
+        </Helmet>
         <AppBanner/>
         <ComicsList/>
         </>
