44b98d794ddc49381ca16e3d35da3fb50afe4026
Sentry11 <99908368+Sentry11@users.noreply.github.com>
2022-11-18 22:56:36 +0200

    Update http.hook.js

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,26 +1,20 @@
 import { useState, useCallback } from "react";
-
-
 export const useHttp = () => {
 
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const [process, setProcess] = useState('waiting');
-
-
-    const request = useCallback( async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
     
+    const request = useCallback( async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
     setLoading(true);
     setProcess('loading');
+        
     try{
-
         const response = await fetch(url, {method, body, headers});
-
         if(!response.ok){
             throw new Error(`Could not fetch ${url}, status: ${response.status}`)
         }
         const data = await response.json();
-
         setLoading(false);
         return data;
 
@@ -30,8 +24,6 @@ export const useHttp = () => {
         setProcess('error');
         throw e;
     }
-    
-
     }, [])
 
 
