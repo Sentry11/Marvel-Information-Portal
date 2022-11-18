c3ee889f8e194752131683e42cc05e6eae5aa5ea
Sentry11 <99908368+Sentry11@users.noreply.github.com>
2022-11-18 22:53:09 +0200

    Update 404.js

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,7 +1,5 @@
 import ErrorMessage from "../errorMessage/ErrorMessage"
 
-
-
 const Page404 = () => {
     return(
         <div>
@@ -9,9 +7,6 @@ const Page404 = () => {
           <p style = {{'textAlign': 'center','fontWeight': 'bold', 'fontSize': '24px'}}> 404. Page doesn'n exist </p>      
           {/* <Link to = {/} style = {{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px', 'marginTop': '30px'}}>Back to main page</Link> */}
         </div>
-        
     )
 }
-
-
 export default Page404;
