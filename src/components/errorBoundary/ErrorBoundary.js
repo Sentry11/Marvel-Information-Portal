8f949fce523e216b6c653938d499c5d7ebf8c708
Sentry11 <99908368+Sentry11@users.noreply.github.com>
2022-11-18 22:51:15 +0200

    Update ErrorBoundary.js

--- src/components/errorBoundary/ErrorBoundary.js
+++ src/components/errorBoundary/ErrorBoundary.js
@@ -5,19 +5,16 @@ class ErrorBoundary extends Component {
     state = {
         error: false
     }
-
     componentDidCatch(error, errorInfo) {
         console.log(error, errorInfo);
         this.setState({
             error: true
         })
     }
-
     render() {
         if (this.state.error) {
             return <ErrorMessage/>
         }
-
         return this.props.children;
     }
 }
