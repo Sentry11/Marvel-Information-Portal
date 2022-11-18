cbae5e7a959f8df8a91f39175017fd3207d2ce4b
Sentry11 <99908368+Sentry11@users.noreply.github.com>
2022-11-19 01:50:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-// import React from 'react';
-// import App from './components/app/App';
-// import './style/style.scss';
-// import {createRoot} from 'react-dom/client';
-
-// const container = document.getElementById('root');
-// const root = createRoot(container);
-
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
- 
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
